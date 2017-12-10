745cbc09c2b84ede88fe3b6fb4b46be4a3401eeb
= <=>
2017-12-10 17:11:19 -0200

    aaa

--- app/src/main/java/com/example/guerra/repmanager_aaa/Tasks.java
+++ app/src/main/java/com/example/guerra/repmanager_aaa/Tasks.java
@@ -1,6 +1,11 @@
 package com.example.guerra.repmanager_aaa;
 
 
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -8,29 +13,69 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.json.*;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+
+import static java.security.AccessController.getContext;
 
 public class Tasks extends AppCompatActivity {
 
-    public String loadJSONFromAsset(String file) {
+    public String loadJSONFromAsset(String path) {
         String json = null;
         try {
 
-            InputStream is = getAssets().open(file);
+
+            File initialFile = new File(path);
+            InputStream is =  new FileInputStream(initialFile);
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
+    }
+
+    public String loadJSONFromAssetreal(String path) {
+        String json = null;
+        try {
+            InputStream is = getAssets().open(path);
 
             int size = is.available();
 
@@ -56,39 +101,234 @@ public class Tasks extends AppCompatActivity {
     public List<String> responsibles = new ArrayList<String>();
     public List<String> tasks = new ArrayList<String>();
     public List<String> dates = new ArrayList<String>();
+    JSONArray taskList;
+    public TextView memberDate;
+    DatePickerDialog.OnDateSetListener mDataSetListener;
+
+    public void setDate(View view){
+
+
+        Log.d("MyApp","suck my dick222");
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dialog = new DatePickerDialog(
+                Tasks.this,
+                android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                mDataSetListener,
+                year, month, day
+        );
+
+
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+    }
 
     public void addTask(View view) {
 
         AlertDialog.Builder mBuilder = new AlertDialog.Builder(Tasks.this);
         View mView = getLayoutInflater().inflate(R.layout.dialog_new_task, null);
 
-        final EditText editTextMember = (EditText) mView.findViewById(R.id.editTextMember);
-        final EditText editDateLimit = (EditText) mView.findViewById(R.id.editDateLimit);
         final EditText editTaskDescription = (EditText) mView.findViewById(R.id.editTaskDescription);
         Button addTask = (Button) mView.findViewById(R.id.addTask);
 
+        final Spinner memberSpinner = (Spinner) mView.findViewById(R.id.memberSpinner);
+        List<String> membersList = new ArrayList<String>();
+
+
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Tasks.this, android.R.layout.simple_spinner_dropdown_item) {
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+
+                View v = super.getView(position, convertView, parent);
+                if (position == getCount()) {
+                    ((TextView)v.findViewById(android.R.id.text1)).setText("");
+                    ((TextView)v.findViewById(android.R.id.text1)).setHint(getItem(getCount())); //"Hint to be displayed"
+                }
+
+                return v;
+            }
+
+            @Override
+            public int getCount() {
+                return super.getCount()-1; // you dont display last item. It is used as hint.
+            }
+
+        };
+
+
+        try {
+
+            JSONObject obj = new JSONObject(loadJSONFromAssetreal("members.json"));
+            JSONArray memberList = obj.getJSONArray("members");
+            for (int i = 0; i < memberList.length(); i++){
+                JSONObject jsonObj = memberList.getJSONObject(i);
+                adapter.add(jsonObj.getString("name"));
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        adapter.add("Membro");
+        memberSpinner.setAdapter(adapter);
+        memberSpinner.setSelection(adapter.getCount());
+
+
+
+        memberDate = (TextView) mView.findViewById(R.id.limiteData);
+
+
+        mDataSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String date = dayOfMonth + "/" + month + "/" + year;
+                memberDate.setText(date);
+            }
+        };
+
+
+        mBuilder.setView(mView);
+        final AlertDialog dialogPop = mBuilder.create();
 
         addTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                if(!editTextMember.getText().toString().isEmpty() && !editDateLimit.getText().toString().isEmpty() && !editTaskDescription.getText().toString().isEmpty()){
+                if(!memberSpinner.getSelectedItem().toString().equals("Membro") && !memberDate.getText().toString().isEmpty() && !editTaskDescription.getText().toString().isEmpty()){
 
+
+                    JSONObject taskObj = new JSONObject();
+
+                    String path = getFilesDir().getAbsolutePath() + File.separator + "RepManager";
+                    File projDir = new File(path);
+                    if (!projDir.exists())
+                        projDir.mkdirs();
+
+
+
+                    try {
+                        JSONObject member = new JSONObject();
+                        member.put("responsible", memberSpinner.getSelectedItem().toString());
+                        member.put("task", editTaskDescription.getText());
+                        member.put("date", memberDate.getText());
+                        taskList.put(member);
+                        taskObj.put("tasks", taskList);
+
+                        File file = new File(path + "/tasks.json");
+                        String[] save = { taskObj.toString() };
+                        Save(file, save);
+
+                    }
+                    catch(Exception e){
+                        e.printStackTrace();
+                    }
+
+
+                    dialogPop.dismiss();
                     Log.d("MyApp","suck my dick");
+
                 }
                 else{
+                    String path = getFilesDir().getAbsolutePath() + File.separator + "RepManager";
 
-                    Log.d("MyApp","fuck you");
+                    File file = new File(path + "/tasks.json");
+                    String [] man = Load(file);
+
+                    Log.d("MyApp",man[0]);
                 }
             }
         });
 
+        dialogPop.show();
 
         mBuilder.setView(mView);
-        AlertDialog dialog = mBuilder.create();
-        dialog.show();
         Log.d("MyApp","I am hereaaaaa");
     }
 
+    public static String[] Load(File file)
+    {
+        FileInputStream fis = null;
+        try
+        {
+            fis = new FileInputStream(file);
+        }
+        catch (FileNotFoundException e) {e.printStackTrace();}
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        String test;
+        int anzahl=0;
+        try
+        {
+            while ((test=br.readLine()) != null)
+            {
+                anzahl++;
+            }
+        }
+        catch (IOException e) {e.printStackTrace();}
+
+        try
+        {
+            fis.getChannel().position(0);
+        }
+        catch (IOException e) {e.printStackTrace();}
+
+        String[] array = new String[anzahl];
+
+        String line;
+        int i = 0;
+        try
+        {
+            while((line=br.readLine())!=null)
+            {
+                array[i] = line;
+                i++;
+            }
+        }
+        catch (IOException e) {e.printStackTrace();}
+        return array;
+    }
+
+    public static void Save(File file, String[] data)
+    {
+        FileOutputStream fos = null;
+        try
+        {
+            fos = new FileOutputStream(file);
+        }
+        catch (FileNotFoundException e) {e.printStackTrace();}
+        try
+        {
+            try
+            {
+                for (int i = 0; i<data.length; i++)
+                {
+                    fos.write(data[i].getBytes());
+                    if (i < data.length-1)
+                    {
+                        fos.write("\n".getBytes());
+                    }
+                }
+            }
+            catch (IOException e) {e.printStackTrace();}
+        }
+        finally
+        {
+            try
+            {
+                fos.close();
+            }
+            catch (IOException e) {e.printStackTrace();}
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,8 +341,9 @@ public class Tasks extends AppCompatActivity {
         ListView listView = (ListView)findViewById(R.id.listView);
 
         try {
-            JSONObject obj = new JSONObject(loadJSONFromAsset("taskList.json"));
-            JSONArray taskList = obj.getJSONArray("tasks");
+            String path = getFilesDir().getAbsolutePath() + File.separator + "RepManager";
+            JSONObject obj = new JSONObject(loadJSONFromAsset(path + "/tasks.json"));
+            taskList = obj.getJSONArray("tasks");
             sizeTaskList = taskList.length();
             for (int i = 0; i < taskList.length(); i++){
 
@@ -115,6 +356,8 @@ public class Tasks extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
 
         CustomAdapter customAdapter = new CustomAdapter();
         listView.setAdapter(customAdapter);
